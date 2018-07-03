85caecab89cec4883cd8002e9d4a1ce3e394fca9
Глеб Завьялов <glebzavalov@macbook-pro-gleb.local>
2018-07-03 18:08:37 +0200

    Initial Commit

---
+++ wellDoneApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  wellDoneApp
+//
+//  Created by Глеб Завьялов on 03/07/2018.
+//  Copyright © 2018 Глеб Завьялов. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
